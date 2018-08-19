48b4c9d8d449cc2d5c6c7bbf3b9905df70053c17
芝田大樹 <shibatadaiki@shibatadaikinoMacBook-Air.local>
2018-08-19 20:54:55 +0900

    環境・初期設定

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,14 @@ module DevExam
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
+    config.generators do |g|
+      g.assets false
+      g.helper false
+      g.jbuilder false
+      g.test_framework false
+    end
   end
 end
